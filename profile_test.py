38f57a97204256541a44c09d42081f7027458afe
Mageshkannan002w <magesh.kothandaraman@goml.io>
2026-07-24 13:36:51 +0530

    modified profiling and added docs

--- profile_test.py
+++ profile_test.py
@@ -6,11 +6,11 @@ def run_profile():
     profiler = cProfile.Profile()
     profiler.enable()
 
-    pytest.main(["tests/unit/test_profiling.py"])
+    pytest.main(["tests/integration/test_endpoint_profiling.py", "tests/unit/test_profiling.py"])
 
     profiler.disable()
     stats = pstats.Stats(profiler)
-    stats.strip_dirs().sort_stats("cumulative").print_stats(10)
+    stats.strip_dirs().sort_stats("cumulative").print_stats(15)
 
 if __name__ == "__main__":
     run_profile()
